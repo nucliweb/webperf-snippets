2e6edb74dcc6b7325f6be1c8105f30d15e91146e
Joan León <joan.leon@gmail.com>
2023-06-02 21:57:04 +0200

    Add Vercel Analytics

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import { Analytics } from '@vercel/analytics/react';
+ 
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+ 
+export default MyApp;
