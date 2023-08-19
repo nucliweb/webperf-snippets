be724dc0869da73dfb934e5457442a0878732c0c
Joan León <joan.leon@gmail.com>
2023-08-20 00:38:44 +0200

    Add RUM DebugBear script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Script from "next/script";
+
 function WebPerfSnippets({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        src="https://cdn.debugbear.com/BK73p0yToVVP.js"
+        strategy="afterInteractive"
+      />
+    </>
+  );
 }
 
 export default WebPerfSnippets;
