de6aad8c9bdfb5e1db205523aa7d8e3cfd06ebe7
Joan León <joan.leon@gmail.com>
2023-09-06 11:40:27 +0200

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,16 @@ function WebPerfSnippets({ Component, pageProps }) {
         src="https://cdn.debugbear.com/BK73p0yToVVP.js"
         strategy="afterInteractive"
       />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-NNX9SYKEV2"></Script>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-NNX9SYKEV2');
+        `}
+      </Script>
     </>
   );
 }
