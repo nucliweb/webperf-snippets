e3f3e4b3d3145b01ba9cdf05e9798bf60dffa3b6
Joan León <joan.leon@gmail.com>
2023-09-13 20:31:22 +0200

    fix(imageLazy): rename file and add permanent redirect

--- next.config.js
+++ next.config.js
@@ -2,8 +2,18 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx'
 })
- 
-module.exports = withNextra()
- 
+
+module.exports = withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/Loading/Find-Above-The-Fold-Lazy-Loades-Images',
+        destination: '/Loading/Find-Above-The-Fold-Lazy-Loaded-Images',
+        permanent: true,
+      },
+    ]
+  },
+})
+
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
