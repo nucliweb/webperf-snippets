15cd206727305298e9affca531afe41ed3ae54b3
Joan Leon <joan.leon@gmail.com>
2026-02-25 08:54:33 +0100

    Remove block code from search indexing

--- next.config.js
+++ next.config.js
@@ -1,24 +1,27 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
 
 module.exports = withNextra({
   async redirects() {
     return [
       {
-        source: '/Loading/Find-Above-The-Fold-Lazy-Loades-Images',
-        destination: '/Loading/Find-Above-The-Fold-Lazy-Loaded-Images',
+        source: "/Loading/Find-Above-The-Fold-Lazy-Loades-Images",
+        destination: "/Loading/Find-Above-The-Fold-Lazy-Loaded-Images",
         permanent: true,
       },
       {
-        source: '/Loading/Inline-Script-Info-and-Size-Including__NEXT_DATA',
-        destination: '/Loading/SSR-Hydration-Data-Analysis',
+        source: "/Loading/Inline-Script-Info-and-Size-Including__NEXT_DATA",
+        destination: "/Loading/SSR-Hydration-Data-Analysis",
         permanent: true,
       },
-    ]
+    ];
   },
-})
+  search: {
+    codeblocks: false,
+  },
+});
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
